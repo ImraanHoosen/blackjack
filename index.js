af79e1b6ba22a42700f9853b07241fa63c6da664
HCodez <imraan.hoosen24@gmail.com>
2024-12-02 14:38:05 +0200

    commentrs added to JS file

--- index.js
+++ index.js
@@ -11,39 +11,47 @@ let isAlive = true
 let message = ""
 
 
-function startGame(){
-    renderGame()
+/**
+ * Starts the game and initializes the rendering of the game state.
+ */
+function startGame() {
+    renderGame()             // Render the initial game state
 }
 
 
-function renderGame(){
-
+/**
+ * Renders the game based on the current game state.
+ */
+function renderGame() {
     sumEl.textContent = "Sum : " + sum
     cardsEl.textContent = "Cards : "    
     for (let i = 0; i < cards.length; i++) {        //array used to display the cards 
     cardsEl.textContent += cards[i] + " "
     }
 
-
+    // Check if the player has blackjack or has gone over
     if (sum <= 20) {
-    message = "Do you want to draw a new card?"
-} else if (sum === 21) {
-    message = "You've got Blackjack!"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game!"
-    isAlive = false
-}
-messageEl.textContent=message
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent=message
 }
 
 
-function newCard(){
-     let card = 7
-     sum += card
-     cards.push(card)       // Push feathure used to push new (third card) card to the "cardsEl.textContent" to display in the game. 
-    //  console.log(cards)
-     renderGame()
+
+/**
+ * Handles the logic for drawing a new card.
+ */
+function newCard() {
+    let card = 7
+    sum += card
+    cards.push(card)       // Push feathure used to push new (third card) card to the "cardsEl.textContent" to display in the game. 
+    renderGame()
 }
 
 
